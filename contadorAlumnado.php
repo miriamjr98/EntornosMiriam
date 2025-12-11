228bac5b4c79a77f0d137d357999bf8326c7dcc4
Miriam Jimenez <miriamjribera@gmail.com>
2025-12-11 10:34:33 +0100

    A24: contador de visitas (cookies) implementados

--- contadorAlumnado.php
+++ contadorAlumnado.php
@@ -1,17 +1,22 @@
 <?php
 date_default_timezone_set('Europe/Madrid');
 
-$fecha=date("d/m/Y H:i:s");
+$fecha = date("d/m/Y H:i:s");
 
-//función isset se usa para verificar si una variable está definida y su
-//valor no es NULL. La función isset devuelve true si la variable está
-//definida y no es nula
-if(isset($_COOKIE["visita"])){
-    //aquí va el código se debéis de programar
+if (isset($_COOKIE["visita"])) {
 
-}else{
-    print "<h2> Hola, aquesta és la primera vegada que entres en aquesta pàgina. </h2>";
-    setcookie("visita","1|" . $fecha);
+    list($visitas, $fecha_anterior) = explode("|", $_COOKIE["visita"]);
+    $visitas = intval($visitas) + 1;
+
+    echo "<h2> Benvingut de nou! Aquesta és la visita número $visitas </h2>";
+    echo "<p>La darrera visita va ser: <strong>$fecha_anterior</strong></p>";
+
+    // Actualizar cookie 
+    setcookie("visita", $visitas . "|" . $fecha);
+
+} else {
+    echo "<h2>Hola, aquesta és la primera vegada que entres en aquesta pàgina.</h2>";
+    setcookie("visita", "1|" . $fecha);
 }
 
 ?>
